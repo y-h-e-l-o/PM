3d4182bb4dc81e372a89cd20a80f71f5708a1f0a
yhlo <yosunshine@mailo.com>
2025-01-13 14:16:00 +0100

    stp

--- pm/settings.py
+++ pm/settings.py
@@ -153,7 +153,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'index/static/index'),
 ]
 if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_ROOT = os.path.join(BASE_DIR, 'index/static/index')
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
