e4935ed6b76eb10de7ff777a0be2e35e429c80f0
yhlo <yosunshine@mailo.com>
2025-01-14 14:58:43 +0100

    grrrt

--- pm/settings.py
+++ pm/settings.py
@@ -96,7 +96,7 @@ WSGI_APPLICATION = 'pm.wsgi.application'
 #}
 
 DATABASES = {
-   ‘default‘: dj_database_url.parse(os.environ.get("DATABASE_URL"))
+   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
