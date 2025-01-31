2d0bb355454c757a6c8cd2e37e621f2af43af536
yhlo <yosunshine@mailo.com>
2025-01-31 17:04:40 +0100

    fr

--- pm/settings.py
+++ pm/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
+   # 'livereload',
     'django.contrib.staticfiles',
     'index.apps.IndexConfig',
     'django_summernote',
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript',
+   # 'livereload.middleware.LiveReloadScript',
 ]
 
 ROOT_URLCONF = 'pm.urls'
@@ -87,13 +87,21 @@ WSGI_APPLICATION = 'pm.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.sqlite3',
-       'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': dj_database_url.config(
+default= 'postgresql://pm:uFJ3V1aHzBcsujQG2b18Fe4kvtLFI353@dpg-cu2hlo1u0jms73d96ff0-a/pmdata_bxh8',
+conn_max_age= 600
+   )
 }
+
+
+
+#DATABASES = {
+#   'default': {
+#       'ENGINE': 'django.db.backends.sqlite3',
+#       'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+#}
 
 #DATABASES = {
 #   'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
@@ -148,11 +156,25 @@ USE_I18N = True
 USE_TZ = True
 
 
+STATIC_URL = '/static/'
+# This production code might break development mode, so we check whether we're in DEBUG mode
+if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
+
+
+
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'index/static/index/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'index/static/index')
+#STATIC_URL = 'index/static/index/'
+#STATIC_ROOT = os.path.join(BASE_DIR, 'index/static/index')
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'index/static/index'),
 #]
